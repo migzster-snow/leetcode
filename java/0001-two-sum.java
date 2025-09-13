02e2aeaf4534c89e2a5334acdd8a43c9f88b5723
Jonah Benedicto <jonah.f.benedicto@gmail.com>
2025-09-13 21:06:31 +1000

    1-two-sum hash map method using java

--- java/0001-two-sum.java
+++ java/0001-two-sum.java
@@ -1,12 +1,27 @@
+// Brute Force Solution
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // Brute Force Solution
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
                     return new int[]{i, j};
                 }
             }
+        }
+        return new int[0];
+    }
+}
+
+// HashMap Solution
+import java.util.HashMap;
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> hmap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (hmap.containsKey(target - nums[i])) {
+                return new int[]{hmap.get(target - nums[i]), i};
+            }
+            hmap.put(nums[i], i);
         }
         return new int[0];
     }
